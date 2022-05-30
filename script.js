504f90076402d821303ba6d8a8612785d328f3d9
tedmudau <106542363+tedmudau@users.noreply.github.com>
2022-05-30 19:05:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-let name = document.getElementById("name").value;
-let email = document.getElementById("email").value;
-let message = document.getElementById("message").value;
-let finalmessage = `Name : ${name} <br>  Email : ${email} <br>  Message : ${message} <br>`;
-
 const btn = document.querySelector('button')
-btn.addEventListener('click',() => {
+const inputs = document.querySelector('form')
+btn.addEventListener('click', () => {
+
+    const name = (inputs.elements["name"].value).trim()
+    const email = (inputs.elements["email"].value).trim()
+    const msg = (inputs.elements["message"].value.trim())
+    const phone = (inputs.elements["phone"].value).trim()
+    if (!name.length > 0 || !email.length > 0 || !msg.length > 0 || !phone.length > 0) {
+        alert("All fields are mandatory")
+        return
+    }
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "maluleke45@gmail.com",
-        Password : "004E15D6F0774E003CBFE518032AA62D5124",
-        To : 'maluleke45@gmail.com',
-        From : "you@isp.com",
-        Subject : "Mail from website",
-        Body : finalmessage
-    }).then(
-      message => alert(message)
-    );
+        Host: "smtp.elasticemail.com",
+        Username: "maluleke45@gmail.com",
+        Password: "004E15D6F0774E003CBFE518032AA62D5124",
+        To: "maluleke45@gmail.com",
+        From: email,
+        Subject: "Contact Us Query By the Customer",
+        Body: msg + "<br>" + name + "<br>" + phone
+    }).then(msg => alert("The email successfully sent"))
 })
